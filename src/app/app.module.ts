2e1cf84047d2c66074838d576a19bde5cce13d54
vasanth rajarathinam <vasanth.rajarathinam@hotmail.com>
2022-03-16 02:37:13 +0000

    addingBaseProject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { DummyComponent } from './dummy/dummy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PaginationComponent,
+    NavbarComponent,
+    SignupComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
